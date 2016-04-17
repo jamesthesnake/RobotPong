6065741f61b70957f7866225d1cf4aa9379e7dbc
jamesthesnake <james.ryan.hennessy@gmail.com>
2016-04-17 01:20:58 -0500

    Added files via upload

--- ardLeapCar.js
+++ ardLeapCar.js
@@ -1,30 +1,35 @@
 var Cylon = require('cylon'),
-    LeapFrame= require('./lib/ardLeapCarFrame.js');
+    LeapFrame= require('./ardLeapCarFrame.js');
 
 Cylon.robot({
 	connections: {
-	    leapmotion : { adapator : 'leapmotion'},
+	    leapmotion : { adapator : 'leapmotion' },
 	    arduino: { adaptor: 'firmata', port: '/dev/cu.usbmodem1411'  }
 	},
            
 	    devices: {
-	    pin: { driver: 'direct-pin', pin: 3  }, // right
-		pin10: { driver: 'direct-pin', pin: 4}, // backward
-		pin11: {driver: 'direct-pin', pin:5}, // forward doesnt work
-		pin12: {driver: 'direct-pin', pin:6} //left
+	    leapmotion: {driver: 'leapmotion' , connection: 'leapmotion'},
+	    pin: { driver: 'direct-pin', pin: 3 , connection:'arduino' }, // right
+		pin10: { driver: 'direct-pin', pin: 4, connection:'arduino'}, // backward
+		pin11: {driver: 'direct-pin', pin:5, connection:'arduino'}, // forward doesnt work
+		pin12: {driver: 'direct-pin', pin:6, connection:'arduino'} //left
 
 	    //    button: { driver: 'button', pin: 5 }
 
 	},
 	    up  :function(my){
             var value=0;
-            if(frame.hands.length>0){
-            ever((1).second(), function(){
+	    
+	    //		    frame= new LeapFrame(data);
+	    // if(frame.hands.length>0 && frame.valid){
+		            ever((1).second(), function(){
 		    my.pin.digitalWrite(value);
 		    value= (value == 0)? 1 : 0;
-		});
-	    }
+		
+				});
 	},
+	    
+    
 	    down :function(my){
             var value=0;
             ever((1).second(), function(){
